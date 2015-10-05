6168aff3ee6366d61fc8490cda368ff6402f64ce
EmperorOfDoom <emperor_of_doom@live.com>
2015-10-05 21:04:28 +0200

    Initialized Project and Mod
    Initialized Project and Mod

---
+++ ChangeRoadHeight/ChangeRoadHeight/ChangeRoadHeight.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ICities;
+
+namespace ChangeRoadHeight
+{
+    public class ChangeRoadHeight : IUserMod
+    {
+        public string Name
+        {
+            get
+            {
+                return "Change Road Height";
+            }
+        }
+        public string Description
+        {
+            get
+            {
+                return "Move existing roads in height.";
+            }
+        }
+    }
+}
